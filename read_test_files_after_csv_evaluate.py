f4d70c483d9c2f741931663a41bd5e70cc6148c0
Thomas Ferte <thomas.ferte@u-bordeaux.fr>
2024-06-10 15:10:18 +0200

    add aggregation of hp

--- read_test_files_after_csv_evaluate.py
+++ read_test_files_after_csv_evaluate.py
@@ -16,9 +16,6 @@ folder_list = [d for d in directories if "GAHPDEF" in d]
 # Get the folder_i based on array_id
 folder_i = folder_list[int(array_id)]
 
-# Create an empty list to store dataframes
-df_list = []
-
 # List all files in the specified folder
 folder_path_glob = folder_path + folder_i + "/test/"
 file_pattern = "**/*.csv"
@@ -28,6 +25,36 @@ files = glob.glob(os.path.join(folder_path_glob, file_pattern), recursive=True)
 importance_files = [path for path in files if "_importance/" in path]
 prediction_files = [path for path in files if "_importance/" not in path]
 
+# List hp files
+folder_path_hp = folder_path + folder_i +"/"
+# List all files in the directory
+all_files = os.listdir(folder_path_hp)
+# Filter out only the .csv files
+hp_files = [file for file in all_files if file.endswith('.csv')]
+
+##### Hyperparameters
+# Create an empty list to store dataframes
+df_list = []
+# Iterate over the files
+for file in hp_files:
+  # Read the data from the file using pandas
+  df_res = pd.read_csv(folder_path_hp + file)
+  # Extract trial and hp_date from the file path
+  # Add trial and hp_date as new columns
+  df_res['file_hp'] = file
+  # Append the dataframe to df_list
+  df_list.append(df_res)
+
+# Concatenate all dataframes in df_list into one
+dfres = pd.concat(df_list, ignore_index=True)
+
+# Save the resulting dataframe as an RDS file
+# dfres.to_csv(folder_path + folder_i + "/" + folder_i + "_combined.csv", index = False)
+dfres.to_csv("/beegfs/tferte/output/aggregated_results/" + folder_i + "_hyperparameters.csv", index = False)
+
+##### Prediction
+# Create an empty list to store dataframes
+df_list = []
 # Iterate over the files
 for file in prediction_files:
   # Read the data from the file using pandas
@@ -48,6 +75,9 @@ dfres = pd.concat(df_list, ignore_index=True)
 # Save the resulting dataframe as an RDS file
 # dfres.to_csv(folder_path + folder_i + "/" + folder_i + "_combined.csv", index = False)
 dfres.to_csv("/beegfs/tferte/output/aggregated_results/" + folder_i + "_combined.csv", index = False)
+
+
+##### Feature importance
 
 if folder_i not in ["2000units_20reservoir", "GeneticSingleIs_GA_20esn", "GeneticSingleIs_GA_20esn_week", "GeneticSingleIs_GA_10esn"]:
     df_list = []
